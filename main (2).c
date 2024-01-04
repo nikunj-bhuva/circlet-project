8f8b315a5b04e9d2492d029749fc7f6a7668260c
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-01-04 11:42:24 +0530

    hii

---
+++ main (2).c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // first question//
+
+    for(int i=1; i<=6; i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    for(int i=1; i<=6; i++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+    for(int i=1; i<=6; i++)
+    {
+        printf("* ");
+    }
+
+    // second question //
+
+    // int num = 30;
+    // int even;
+    // int odd;
+    // {
+    //     if (num %2 == 0)
+    //     {
+    //         printf("THIS NUMBER IS EVEN");
+    //     }
+    //     else if (num  == 0)
+    //     {
+    //         printf("THIS NUMBER IS ODD");
+    //     }
+    // }
+
+    return 0;
+}
